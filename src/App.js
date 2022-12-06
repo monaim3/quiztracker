897cab23c04af8079273384179d0f53ae613a549
monaim3 <monaimmukul75@gmail.com>
2022-12-06 12:05:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  useParams,
+} from "react-router-dom";
+import Blog from './Component/Blog/Blog';
+import Statistics from './Component/Statistics/Statistics';
+import About from './Component/About/About';
+import Main from './Component/Main/Main';
+import QuizDeatils from './Component/QuizDeatils/QuizDeatils';
+import Home from './Component/Home/Home';
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: 'blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: 'about',
+          element: <About></About>
+        },
+        {
+          path: 'Statistics',
+          loader:async()=>{
+            return fetch('https://openapi.programming-hero.com/api/quiz')
+          },
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/quiz/:id',
+          loader: async ({ params }) => {
+            return fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`)
+          },
+          element: <QuizDeatils></QuizDeatils>
+        },
+      ]
+    },
+  
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
